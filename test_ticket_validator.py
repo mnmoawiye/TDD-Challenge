cc06f908ac391f18be9fd6d492347966ec334ff9
mnmoawiye <masoudmnm21@gmail.com>
2026-04-14 12:22:59 +0000

    commited

--- test_ticket_validator.py
+++ test_ticket_validator.py
@@ -3,13 +3,25 @@ from ticket_validator import validate_ticket, get_ticket_tier, calculate_total
 
 # Validate Ticket
 def test_validation():
-    assert validate_ticket("TK928450") == True
+    assert validate_ticket("TK123456") == True
 
 def test_too_long():
     assert validate_ticket("TK1234567") == False
 
 def test_wrong_start():
     assert validate_ticket("QQ123456") == False
+
+@pytest.mark.parametrize("code, result", [
+    ("TK012345", "General"),
+    ("TK456789", "VIP"),
+    ("TK789123", "Platinum")
+])
+def test_tiers(code, result):
+    assert get_ticket_tier(code) == result
+
+def test_invalid_ticket():
+    with pytest.raises(ValueError):
+        get_ticket_tier("INVALID")
 
 
 # calculate_total
--- ticket_validator.py
+++ ticket_validator.py
@@ -1,9 +1,28 @@
 
 def validate_ticket(code):
-    pass
+    if not isinstance(code, str):
+        raise TypeError
+    if len(code) != 8:
+        return False
+    if not code.startswith("TK"):
+        return False
 
 def get_ticket_tier(code):
-    pass
+    if not validate_ticket(code):
+        raise ValueError
+    num = int(code[2])
+
+    if num <= 3:
+        return "General"
+    elif num <=6:
+        return "VIP"
+    else:
+        return "Platinum"
 
 def calculate_total(prices, discount=0):
-    pass
+    if not isinstance(prices, list):
+        raise TypeError
+    if not prices:
+        raise ValueError
+    total = sum (prices)
+    return round(total)
